66d083344c857b2b48ea8bcbdfccf1657f3849b4
Eduardo Baroncini <ebaroncini@gmail.com>
2021-01-30 13:40:55 -0300

    cambios en el formato de about y services

--- src/app/service/service.model.ts
+++ src/app/service/service.model.ts
@@ -1,18 +1,21 @@
 export class Service {
     public id: string;
     public title: string;
+    public image: string;
     public images: string[];
     public largeDescription: string;
     public details: string[];
 
     constructor(id: string,
         title: string,
+        image: string,
         images: string[],
         largeDescription: string,
         details: string[]
     ) {
         this.id = id;
         this.title = title;
+        this.image = image;
         this.images = images;
         this.largeDescription = largeDescription;
         this.details = details;
