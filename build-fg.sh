41e19fc69c8a851e1e6287fb4383bce1ad80a808
Eduardo Baroncini <ebaroncini@gmail.com>
2021-04-11 15:58:29 -0300

    agregamos imagen a contacto

--- build-fg.sh
+++ build-fg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ng build --prod --base-href https://flaviagiustino.com.ar/ --aot && rm flaviagiustino.com.ar/*.* && cp dist/angular/*.* flaviagiustino.com.ar/ && cp flaviagiustino.com.ar/index.html flaviagiustino.com.ar/404.html && echo OK
+ng build --prod --base-href https://flaviagiustino.com.ar/ --aot && rm flaviagiustino.com.ar/*.* && cp dist/angular/*.* flaviagiustino.com.ar/ && cp flaviagiustino.com.ar/index.html flaviagiustino.com.ar/404.shtml && echo OK
 
--- src/app/contact/contact.model.ts
+++ src/app/contact/contact.model.ts
@@ -1,14 +1,21 @@
 export class Contact {
     public title: string;
     public description: string;
+    public image: string;
     public phone: string;
     public email: string;
     public social: any[];
 
-    constructor(phone: string,
+    constructor(title: string,
+        description: string,
+        image: string,
+        phone: string,
         email: string,
         social: any[]
     ) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
         this.phone = phone;
         this.email = email;
         this.social = social;
