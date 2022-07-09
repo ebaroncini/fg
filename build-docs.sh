1505cc35674613768e2a44a9ede0b05c568a839b
Eduardo Baroncini <ebaroncini@gmail.com>
2022-07-09 20:30:55 -0300

    volvi a buildear porque paltaba incluir el custom-gtag.js

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ng build --prod --base-href https://ebaroncini.github.io/fg/ --aot && rm docs/*.* && cp dist/angular/*.* docs/ && cp docs/index.html docs/404.html && echo OK
+ng build --prod --base-href https://ebaroncini.github.io/fg/ --aot && rm docs/*.* &&  rm docs/js/*.* && cp dist/angular/*.* docs/ && cp dist/angular/js/*.* docs/js && cp docs/index.html docs/404.html && echo OK
 
